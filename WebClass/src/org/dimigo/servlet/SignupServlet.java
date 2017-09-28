aeffae211b7cda137a661645948b14a1320540e1
cuizz <cuizz@DESKTOP-Q1ORL3S>
2017-09-28 14:31:25 +0900

    ;

--- WebClass/src/org/dimigo/servlet/SignupServlet.java
+++ WebClass/src/org/dimigo/servlet/SignupServlet.java
@@ -1,6 +1,8 @@
 package org.dimigo.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +21,7 @@ public class SignupServlet extends HttpServlet {
      */
     public SignupServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
@@ -28,6 +30,14 @@ public class SignupServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String data = request.getParameter("data");		
+
+        request.setAttribute("dataobject", data); //객체를 request객체에 담음 (data가 문자열이 아니어도 가능)
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("jsp005_call_jsp_from_servlet.jsp");
+
+        dispatcher.forward(request, response);
 	}
 
 	/**
